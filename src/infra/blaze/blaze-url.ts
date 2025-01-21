c4210f4cfbf61e122d059fdf559a728055aa03ba
Vinicius Silva <viniciusgdr@outlook.com>
2025-01-21 20:21:03 -0300

    chore: update blaze urls

--- src/infra/blaze/blaze-url.ts
+++ src/infra/blaze/blaze-url.ts
@@ -3,6 +3,6 @@ export const getBlazeUrl = (type: 'games' | 'general'): string => {
     case 'games':
       return 'wss://api-gaming.blaze.bet.br/replication/?EIO=3&transport=websocket'
     case 'general':
-      return 'wss://api-gaming.blaze.bet.br/replication/?EIO=3&transport=websocket'
+      return 'wss://api-v2.blaze.bet.br/replication/?EIO=3&transport=websocket'
   }
 }
